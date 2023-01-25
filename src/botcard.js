4e1e76a3782301d695d18ae76febb283900b42d1
Andrew Kibiwott <kibiandrew@gmail.com>
2023-01-26 01:22:21 +0300

    update

---
+++ src/botcard.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
+function BotCard({bot, army,setArmy,setBots}) {
+   function handleClick(){
+    if (army.find((botArmy) => botArmy.id === bot.id)) {
+      setArmy((army) => army.filter((botArmy) => botArmy.id !== bot.id));
+    } else {
+      setArmy((army) => [...army, bot]);
+    }
+   
+   }
+
+
+  function handleDelete(){
+    fetch(`http://localhost:8002/bots/${bot.id}`,{
+     method: "DELETE" ,
+    })
+    .then((res) => res.json())
+    .then(() => {
+      setBots((bots) => bots.filter((botArmy) => botArmy.id !== bot.id));
+    });
+  }
+  return (
+    <div className="ui column">
+      <div
+        className="ui card"
+        key={bot.id}
+        onClick={handleClick}
+      >
+        <div className="image">
+          <img alt="oh no!" src={bot.avatar_url} />
+        </div>
+        <div className="content">
+          <div className="header">
+            {bot.name}
+            <i className={botTypeClasses[bot.bot_class]} />
+          </div>
+          <div className="meta text-wrap">
+            <small>{bot.catchphrase}</small>
+          </div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat" />
+            {bot.health}
+          </span>
+
+          <span>
+            <i className="icon lightning" />
+            {bot.damage}
+          </span>
+          <span>
+            <i className="icon shield" />
+            {bot.armor}
+          </span>
+          <span>
+            <div className="ui center aligned segment basic">
+              <button
+                className="ui mini red button"
+                onClick={handleDelete}
+              >
+                x
+              </button>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BotCard;
